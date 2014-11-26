bba5b21f0f1faa0c5b4af973f7a30ffe59a885a2
Josh Zoff <joshzoff@gmail.com>
2014-11-26 09:35:52 -0500

    Add module to mock state in tests

--- test/frontend/karma.conf.js
+++ test/frontend/karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function(config){
       'client/vendor/angular-animate/angular-animate.js',
       'client/vendor/angular-mocks/angular-mocks.js',
       'client/js/**/*.js',
-      'test/frontend/unit/**/*.js'
+      'test/frontend/unit/**/*.js',
+      'test/frontend/stateMock.js'
     ],
 
     frameworks: ['jasmine'],
