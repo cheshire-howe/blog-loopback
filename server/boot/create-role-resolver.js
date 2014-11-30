7f8929964d95b69656936b15e44a20f2cf90e14e
Josh Zoff <joshzoff@gmail.com>
2014-11-29 22:16:35 -0500

    Fix for hole in deleting all comments
    Wrote a custom role resolver to check if a user is the owner of a post
    before its related comments are deleted.
    
    This is changed from delete being allowed if the post is the owner of
    the comments, which is always true when deleting a post, therefore an
    api call of delete all comments with a readily available postId could
    be called by any logged in user.

--- server/boot/create-role-resolver.js
+++ server/boot/create-role-resolver.js
@@ -1,6 +1,35 @@
 var debug = require('debug')('boot:create-role-resolver');
 
 module.exports = function(app) {
-  //var Role = app.models.Role;
+  var Role = app.models.Role;
+  Role.registerResolver('postAuthor', function(role, context, cb) {
+    function reject() {
+      process.nextTick(function() {
+        cb(null, false);
+      });
+    }
+    
+    if (context.modelName !== 'Post') {
+      // the target model is not a Post
+      return reject();
+    }
+    
+    var userId = context.accessToken.userId;
+    if (!userId) {
+      return reject(); // do not allow anonymous
+    }
+    // if the userId is not the Post.userId, reject
+    context.model.findById(context.modelId, function(err, post) {
+      if (err || !post) {
+        return reject();
+      }
+      
+      if (post.userId.toString() !== userId.toString()) {
+        return cb(null, false);
+      } else {
+        return cb(null, true);
+      }
+    });
+  });
   
 };
