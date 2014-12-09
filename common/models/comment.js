c92e8695144c17d8d728e40dde25a8892ea39f99
Josh Zoff <joshzoff@gmail.com>
2014-12-09 11:39:21 -0500

    Added filter to include username in Comment remote method

--- common/models/comment.js
+++ common/models/comment.js
@@ -1,6 +1,14 @@
 module.exports = function(Comment) {
   Comment.getCommentsByPost = function(postId, cb) {
-    Comment.find({where: {postId: postId}}, cb);
+    Comment.find({
+      where: {postId: postId},
+      include: {
+        relation: 'user',
+        scope: {
+          fields: ['username']
+        }
+      }
+    }, cb);
   };
   Comment.remoteMethod('getCommentsByPost', {
     accepts: [
